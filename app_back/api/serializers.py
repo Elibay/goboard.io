d571de7c0f7fb73070c82b897f0d0a7d52a3e1e0
Sanzhik <sanzhar.bidaibek@gmail.com>
2019-08-16 19:33:18 +0300

    added authorizarion and upgraded models

--- app_back/api/serializers.py
+++ app_back/api/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 
-from api.models import Player, Lobby
+from api.models import Player, Lobby, Participation
 
 
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = '__all__'
+
+
+class ParticipationSerializer(serializers.ModelSerializer):
+    player = PlayerSerializer()
+
+    class Meta:
+        model = Participation
+        fields = ['id', 'lobby', 'player', 'is_admin', 'is_current']
 
 
 class LobbySerializer(serializers.ModelSerializer):
